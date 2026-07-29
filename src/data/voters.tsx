5bca0ac08ccacb34245bfc91e4cfa3e398a34fbb
Raju Kumar <rajukumarsdr95@gmail.com>
2026-07-29 23:14:04 +0530

    add voters welcome page

---
+++ src/data/voters.tsx
@@ -0,0 +1,20 @@
+export const DEMO_VOTERS = [
+  {
+    id: "VOTER001",
+    name: "Aarav Sharma",
+    pin: "1234",
+    hasVoted: false,
+  },
+  {
+    id: "VOTER002",
+    name: "Ananya Singh",
+    pin: "5678",
+    hasVoted: true,
+  },
+  {
+    id: "VOTER003",
+    name: "Rohan Verma",
+    pin: "9012",
+    hasVoted: false,
+  },
+];
